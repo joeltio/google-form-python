8f156daf759c696105c8899527fae216036f9c97
Joel Tio <joeltiojinhon@gmail.com>
2019-02-06 01:04:00 +0800

    Refactor XPathFreebirdDiv tests

--- tests/unit_tests/test_utils.py
+++ tests/unit_tests/test_utils.py
@@ -2,67 +2,70 @@ import lxml.etree as etree
 from googleform.utils import xpath_freebird_div
 
 
+def pytest_generate_tests(metafunc):
+    funcarglist = metafunc.cls.params[metafunc.function.__name__]
+    argnames = sorted(funcarglist[0])
+    metafunc.parametrize(argnames, [
+        [funcargs[name] for name in argnames]
+        for funcargs in funcarglist])
+
+
+def make_freebird_tree(class_name="", inner_tag="div"):
+    class_attribute = "class='freebirdFormviewerViewItems{}'".format(
+        class_name)
+
+    if not class_name:
+        class_attribute = ""
+
+    html = """<div>
+        <{0} {1}></{0}>
+    </div>""".format(inner_tag, class_attribute)
+
+    return etree.fromstring(html)
+
+
 class TestXPathFreebirdDiv:
-    def test_does_not_contain(self):
-        html = "<div><div></div></div>"
-        tree = etree.fromstring(html)
+    params = {
+        "test_does_not_contain": [
+            dict(tree=make_freebird_tree(), find_name="foo"),
+            dict(tree=make_freebird_tree("bar"), find_name="foo"),
+            dict(tree=make_freebird_tree("foo", "label"), find_name="foo"),
+        ],
+        "test_valid": [
+            dict(tree=make_freebird_tree("foo"), find_name="foo"),
+        ],
+        "test_not_exact_by_default": [
+            dict(tree=make_freebird_tree("foo bar"), find_name="foo"),
+        ],
+        "test_exact_class_invalid": [
+            dict(tree=make_freebird_tree("foo bar"), find_name="foo"),
+        ],
+        "test_exact_class_valid": [
+            dict(tree=make_freebird_tree("foo"), find_name="foo"),
+        ]
+    }
 
-        result = xpath_freebird_div(tree, "abc")
+    def test_does_not_contain(self, tree, find_name):
+        result = xpath_freebird_div(tree, find_name)
 
         assert result == []
 
-    def test_wrong_class_suffix(self):
-        html = "<div><div class='freebirdFormviewerViewItemscba'></div></div>"
-        tree = etree.fromstring(html)
-
-        result = xpath_freebird_div(tree, "abc")
-
-        assert result == []
-
-    def test_different_tag(self):
-        html = """<div>
-            <label class='freebirdFormviewerViewItemsabc'></label>
-        </div>"""
-        tree = etree.fromstring(html)
-
-        result = xpath_freebird_div(tree, "abc")
-
-        assert result == []
-
-    def test_valid(self):
-        html = "<div><div class='freebirdFormviewerViewItemsabc'></div></div>"
-        tree = etree.fromstring(html)
-
-        result = xpath_freebird_div(tree, "abc")
+    def test_valid(self, tree, find_name):
+        result = xpath_freebird_div(tree, find_name)
 
         assert len(result) == 1
 
-    def test_not_exact_by_default(self):
-        html = """<div>
-            <div class='freebirdFormviewerViewItemsabc foo'></div>
-        </div>"""
-        tree = etree.fromstring(html)
-
-        result = xpath_freebird_div(tree, "abc")
+    def test_not_exact_by_default(self, tree, find_name):
+        result = xpath_freebird_div(tree, find_name)
 
         assert len(result) == 1
 
-    def test_exact_class_with_multiple_classes(self):
-        html = """<div>
-            <div class='freebirdFormviewerViewItemsabc foo'></div>
-        </div>"""
-        tree = etree.fromstring(html)
-
-        result = xpath_freebird_div(tree, "abc", exact=True)
+    def test_exact_class_invalid(self, tree, find_name):
+        result = xpath_freebird_div(tree, find_name, exact=True)
 
         assert result == []
 
-    def test_exact_class_valid(self):
-        html = """<div>
-            <div class='freebirdFormviewerViewItemsabc'></div>
-        </div>"""
-        tree = etree.fromstring(html)
-
-        result = xpath_freebird_div(tree, "abc", exact=True)
+    def test_exact_class_valid(self, tree, find_name):
+        result = xpath_freebird_div(tree, find_name, exact=True)
 
         assert len(result) == 1
