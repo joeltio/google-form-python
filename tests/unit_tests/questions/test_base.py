682b6d72e2b410ad0b86856ef37dac85892f87f9
Joel Tio <joeltiojinhon@gmail.com>
2019-02-06 16:24:15 +0800

    Remove datafiles from question test base

--- tests/unit_tests/questions/test_base.py
+++ tests/unit_tests/questions/test_base.py
@@ -11,41 +11,37 @@ FIXTURE_DIR = os.path.join(
     'test_files',
 )
 
-QUESTION_INFO = os.path.join(FIXTURE_DIR, "question_info.json")
 
-ALL_QUESTIONS = pytest.mark.datafiles(
-    os.path.join(FIXTURE_DIR, "checkbox.html"),
-    os.path.join(FIXTURE_DIR, "date_year_time.html"),
-    os.path.join(FIXTURE_DIR, "dropdown.html"),
-    os.path.join(FIXTURE_DIR, "duration.html"),
-    os.path.join(FIXTURE_DIR, "long_text.html"),
-    os.path.join(FIXTURE_DIR, "radio_scale.html"),
-    os.path.join(FIXTURE_DIR, "radio_select.html"),
-    os.path.join(FIXTURE_DIR, "short_text.html"),
-    os.path.join(FIXTURE_DIR, "time.html"),
-)
+@pytest.fixture
+def question_paths():
+    return [
+        os.path.join(FIXTURE_DIR, "checkbox.html"),
+        os.path.join(FIXTURE_DIR, "date_year_time.html"),
+        os.path.join(FIXTURE_DIR, "dropdown.html"),
+        os.path.join(FIXTURE_DIR, "duration.html"),
+        os.path.join(FIXTURE_DIR, "long_text.html"),
+        os.path.join(FIXTURE_DIR, "radio_scale.html"),
+        os.path.join(FIXTURE_DIR, "radio_select.html"),
+        os.path.join(FIXTURE_DIR, "short_text.html"),
+        os.path.join(FIXTURE_DIR, "time.html"),
+    ]
 
 
-@pytest.mark.datafiles(QUESTION_INFO)
-@ALL_QUESTIONS
-def test_get_question_title(datafiles):
-    question_info_basename = os.path.basename(QUESTION_INFO)
-    question_info_filename = next(filter(
-        lambda x: x.basename == question_info_basename,
-        datafiles.listdir()
-    ))
+@pytest.fixture
+def question_info():
+    path = os.path.join(FIXTURE_DIR, "question_info.json")
 
-    question_info = json.load(question_info_filename)
+    with open(path, "r") as f:
+        return json.load(f)
 
-    for path in datafiles.listdir():
-        # Ignore the question info file
-        if path.basename == question_info_basename:
-            continue
 
+def test_get_question_title(question_paths, question_info):
+    for path in question_paths:
         with open(path, "r") as f:
             html = f.read()
 
         tree = etree.HTML(html)
-        expected_title = question_info[path.basename]["title"]
+        basename = os.path.basename(path)
+        expected_title = question_info[basename]["title"]
 
         assert get_question_title(tree) == expected_title
