101c0b38bc943242d14e6172c184575cefab8b07
Joel Tio <joeltiojinhon@gmail.com>
2019-02-09 12:48:58 +0800

    Add test for identifying time questions

---
+++ tests/unit_tests/questions/test_time.py
@@ -0,0 +1,45 @@
+import pytest
+
+from googleform.questions.time import TimeQuestion
+
+
+@pytest.fixture(scope="module")
+def time_paths(fixture_path):
+    return [
+        fixture_path("time.html"),
+    ]
+
+
+@pytest.fixture(scope="module")
+def not_time_paths(fixture_path):
+    return [
+        fixture_path("checkbox.html"),
+        fixture_path("date_time.html"),
+        fixture_path("date_year_time.html"),
+        fixture_path("date_year.html"),
+        fixture_path("date.html"),
+        fixture_path("dropdown.html"),
+        fixture_path("duration.html"),
+        fixture_path("radio_list.html"),
+        fixture_path("radio_scale.html"),
+        fixture_path("short_text.html"),
+        fixture_path("long_text.html"),
+    ]
+
+
+@pytest.fixture(scope="module")
+def time_questions(get_question, time_paths):
+    return list(map(get_question, time_paths))
+
+
+@pytest.fixture(scope="module")
+def not_time_questions(get_question, not_time_paths):
+    return list(map(get_question, not_time_paths))
+
+
+def test_distinguish_time(time_questions, not_time_questions):
+    for question in time_questions:
+        assert TimeQuestion.is_this_question(question["tree"]) is True
+
+    for question in not_time_questions:
+        assert TimeQuestion.is_this_question(question["tree"]) is False
