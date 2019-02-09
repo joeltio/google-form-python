c861da5934fd70fb395d9d3cd46d96d425e2c8ec
Joel Tio <joeltiojinhon@gmail.com>
2019-02-09 15:16:41 +0800

    Add test for create_payload

---
+++ tests/unit_tests/test_question.py
@@ -0,0 +1,27 @@
+from googleform.questions.base import Question
+from googleform.question import create_payload
+
+
+def test_create_payload_combines_question_serialization():
+    class MyQuestion(Question):
+        def __init__(self, id, answer):
+            self.id = id
+            self._answer = answer
+
+        @staticmethod
+        def is_this_question(tree):
+            return True
+
+        def serialize(self):
+            return {
+                self.id: self._answer
+            }
+
+    questions = [MyQuestion("entry.1", "abc"), MyQuestion("entry.2", "cba")]
+
+    expected_payload = {
+        "entry.1": "abc",
+        "entry.2": "cba",
+    }
+
+    assert create_payload(questions) == expected_payload
