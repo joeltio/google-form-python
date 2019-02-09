e961b2ee39e25f9753e6a322f85f88137d69741e
Joel Tio <joeltiojinhon@gmail.com>
2019-02-09 13:17:50 +0800

    Add test for fragments for create response url

--- tests/unit_tests/test_form.py
+++ tests/unit_tests/test_form.py
@@ -25,6 +25,13 @@ class TestCreateResponseURL:
                        "DS3QAQFrMBtj0bJZVGHfjCgxmJgQ9FCXco1Xw/formResponse")
         assert create_response_url(url) == correct_url
 
+    def test_url_with_fragment(self):
+        url = ("https://docs.google.com/forms/d/e/1FAIpJLSfOjI2wTW7w6DS3QAQF"
+               "rMBtj0bJZVGHfjCgxmJgQ9FCXco1Xw/viewform#asdf")
+        correct_url = ("https://docs.google.com/forms/d/e/1FAIpJLSfOjI2wTW7w6"
+                       "DS3QAQFrMBtj0bJZVGHfjCgxmJgQ9FCXco1Xw/formResponse")
+        assert create_response_url(url) == correct_url
+
     def test_url_without_viewform_with_trailing_slash(self):
         url = ("https://docs.google.com/forms/d/e/1FAIpJLSfOjI2wTW7w6DS3QAQF"
                "rMBtj0bJZVGHfjCgxmJgQ9FCXco1Xw/")
