eb787064911a40dd1e7979a4a56e026cf9bcbda6
Joel Tio <joeltiojinhon@gmail.com>
2019-02-12 11:10:32 +0800

    Add test for getting the questions from the form html

--- tests/unit_tests/test_question.py
+++ tests/unit_tests/test_question.py
@@ -1,5 +1,45 @@
+import pytest
+import os
+import lxml.etree as etree
+import json
+
 from googleform.questions.base import Question
-from googleform.question import create_payload
+from googleform.question import create_payload, get_questions
+
+
+FIXTURE_DIR = os.path.join(
+    os.path.dirname(os.path.realpath(__file__)),
+    'test_files',
+)
+
+
+@pytest.fixture(scope="module")
+def fixture_path():
+    def _fixture_path(*args):
+        return os.path.join(FIXTURE_DIR, *args)
+
+    return _fixture_path
+
+
+@pytest.fixture
+def form_tree(fixture_path):
+    path = fixture_path("form.html")
+
+    with open(path, "r") as f:
+        return etree.HTML(f.read())
+
+
+@pytest.fixture
+def question_info(fixture_path):
+    path = fixture_path("question_info.json")
+
+    with open(path, "r") as f:
+        return json.load(f)
+
+
+def test_get_questions(form_tree, question_info):
+    questions = get_questions(form_tree)
+    assert len(question_info) == len(questions)
 
 
 def test_create_payload_combines_question_serialization():
