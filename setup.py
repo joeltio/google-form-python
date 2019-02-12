a6906497ca4ba2b362dcec02499c2a1c3ff7c662
Joel Tio <joeltiojinhon@gmail.com>
2019-02-12 21:32:24 +0800

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+requirements = [
+    "requests",
+    "lxml",
+]
+
+packages = ["googleform"]
+
+setuptools.setup(
+    name="googleform",
+    version="0.0.1a1",
+    author="Joel Tio",
+    author_email="joeltiojinhon@gmail.com",
+    description="Submit Google Forms with magic",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/joeltio/google-form-python",
+    packages=packages,
+    install_requires=requirements,
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3.7",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
