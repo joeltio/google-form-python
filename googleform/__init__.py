11a7a24dd70c78902937c9238493c45c173299c5
Joel Tio <joeltiojinhon@gmail.com>
2019-02-14 14:32:12 +0800

    Update googleform to be able to import questions

--- googleform/__init__.py
+++ googleform/__init__.py
@@ -1,2 +1,12 @@
 from googleform.api import get
 from googleform.form import GoogleForm
+
+from googleform.questions.checkbox import CheckboxQuestion
+from googleform.questions.date import DateQuestion
+from googleform.questions.dropdown import DropdownQuestion
+from googleform.questions.duration import DurationQuestion
+from googleform.questions.long_text import LongTextQuestion
+from googleform.questions.radio_list import RadioListQuestion
+from googleform.questions.radio_scale import RadioScaleQuestion
+from googleform.questions.short_text import ShortTextQuestion
+from googleform.questions.time import TimeQuestion
--- tests/system_tests/test_googleform.py
+++ tests/system_tests/test_googleform.py
@@ -1,9 +1,41 @@
+import pytest
+
 import googleform
+import googleform.questions as gfquestions
 
 
-def test_googleform_imports():
+@pytest.fixture
+def question_types():
+    return {
+        gfquestions.checkbox.CheckboxQuestion,
+        gfquestions.date.DateQuestion,
+        gfquestions.dropdown.DropdownQuestion,
+        gfquestions.duration.DurationQuestion,
+        gfquestions.long_text.LongTextQuestion,
+        gfquestions.radio_list.RadioListQuestion,
+        gfquestions.radio_scale.RadioScaleQuestion,
+        gfquestions.short_text.ShortTextQuestion,
+        gfquestions.time.TimeQuestion
+    }
+
+
+def test_googleform_imports(question_types):
     assert googleform.get == googleform.api.get
     assert googleform.GoogleForm == googleform.form.GoogleForm
+
+    assert googleform.CheckboxQuestion is gfquestions.checkbox.CheckboxQuestion
+    assert googleform.DateQuestion is gfquestions.date.DateQuestion
+    assert googleform.DropdownQuestion is gfquestions.dropdown.DropdownQuestion
+    assert googleform.DurationQuestion is gfquestions.duration.DurationQuestion
+    assert googleform.LongTextQuestion is \
+        gfquestions.long_text.LongTextQuestion
+    assert googleform.RadioListQuestion is \
+        gfquestions.radio_list.RadioListQuestion
+    assert googleform.RadioScaleQuestion is \
+        gfquestions.radio_scale.RadioScaleQuestion
+    assert googleform.ShortTextQuestion is \
+        gfquestions.short_text.ShortTextQuestion
+    assert googleform.TimeQuestion is gfquestions.time.TimeQuestion
 
 
 def test_googleform_get(requests_mock, form_html, form_info):
@@ -15,3 +47,13 @@ def test_googleform_get(requests_mock, form_html, form_info):
     assert form.title == form_info["title"]
     assert form.description == form_info["description"]
     assert len(form.questions) == form_info["number_of_questions"]
+
+
+@pytest.fixture
+def form(form_html):
+    return googleform.GoogleForm(form_html)
+
+
+def test_identify_googleform_question(form, question_types):
+    for question in form.questions:
+        assert question.__class__ in question_types
